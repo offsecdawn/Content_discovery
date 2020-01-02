8f2f5da37d4638290a103e104d874307c8a6ca3b
offsecdawn <51472598+offsecdawn@users.noreply.github.com>
2020-01-02 11:34:05 +0530

    Update 1script_to_find_working_curls.sh

--- 1script_to_find_working_curls.sh
+++ 1script_to_find_working_curls.sh
@@ -9,8 +9,8 @@ fi
 rm -f valid_http_urls.txt
 for i in  $(cat $1)
 do
-	response_code=`curl -k -i --silent  $i| tac| tac | head -n 1 | cut -d' ' -f2`
-	#echo $i $response_code
+	response_code=`curl -k -i --silent  $i --parallel-max 50 --connect-timeout 3 | tac| tac | head -n 1 | cut -d' ' -f2`
+	echo $i $response_code
 	if [ $response_code = 200 ] || [ $response_code = 301 ] || [ $response_code = 302 ] || [ $response_code = 403 ]; then
 	echo $i >> valid_http_urls.txt
 	fi
