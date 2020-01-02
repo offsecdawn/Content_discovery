e6af20b66986cf7be0c31f327471a2beff0de962
offsecdawn <51472598+offsecdawn@users.noreply.github.com>
2020-01-02 12:34:03 +0530

    Update 2ffuf_script.sh

--- 2ffuf_script.sh
+++ 2ffuf_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ -z "$1"];
 then
-echo "Usage:./ffuf_script.sh $wordlist $http_file_name $response_code_to_filter"
+echo "Usage:./ffuf_script.sh wordlist"
 exit
 fi
 #echo "./ffuf_script.sh" $1 $2
 source ~/.bash_profile
-for i in $(cat $2)
+for i in $(cat valid_http_urls.txt)
 do
-	echo $i
-	ffuf -w $1 -u $i/FUZZ -fc $3 -t 500
+        echo $i
+        ffuf -w $1 -u $i/FUZZ -mc 200,204,301,302,307 -t 500
 done
